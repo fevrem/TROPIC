2c91175258e8d2c38567a0c231aea089520b53aa
Martin Fevre <fevremartin2@gmail.com>
2020-03-11 12:32:11 -0400

    Create plt_settings.m

---
+++ +Anim/plt_settings.m
@@ -0,0 +1,8 @@
+function [interpreter,linewidth,fontname,fontsize] = plt_settings()
+
+interpreter = 'latex';                   
+fontname    = 'Helvetica';
+linewidth   = 2;
+fontsize    = 16;
+
+end
