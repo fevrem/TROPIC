e1a344dd128d1aaada9a7b3e27f369fb1d8f9b5f
Martin Fevre <fevremartin2@gmail.com>
2020-03-11 12:36:53 -0400

    Update main.m

--- examples/spatial-12-dof-biped/main.m
+++ examples/spatial-12-dof-biped/main.m
@@ -1,21 +1,293 @@
-%% LOAD MODEL & DERIVE DYNAMICS
+%% LOAD DYNAMICAL SYSTEM
 
-clear ; clc ; close all
+clear all; clc; close all;
 
-% must run from PROSIM-root folder
-PROSIM_addpath('casadi');
+[rbm] = ld_model(...
+    {'model',@Model.spatial_12_dof_biped},...
+    {'debug',false});
+ 
 
-cd Spatial_5_link_biped
+% SPECIFY CONTACT
+
+% Right leg end
+rbm.Contacts{1} = Contact(rbm, 'Point',...
+    {'Friction', true},...
+    {'FrictionCoefficient', 0.6},...
+    {'FrictionType', 'Pyramid'},...
+    {'ContactFrame', rbm.BodyPositions{12,2}})
+
+
+% Left leg end
+% rbm.Contacts{2} = Contact(rbm, 'Point',...
+%     {'Friction', true},...
+%     {'FrictionCoefficient', 0.6},...
+%     {'FrictionType', 'Cone'},...
+%     {'ContactFrame', rbm.BodyPositions{9,2}});
+
+
+%
+% CREATE EMPTY NLP
 
 %{
-    dynamics: 'hybrid', 'continuous'
-    symbolic: 'matlab', 'casadi'
+- nfe: 
+    number of finite elements
+- linear_solver:
+    options: 'ma57', 'ma27', 'mumps'
+- quadrature:
+    rule of integration for collocation scheme
+    options: 'trapezoidal'
+- constraint_tol:
+    options: double
 %}
-[rbm] = ld_model(...
-    
-    {'model',@Model.spatial_5_link},...
-    {'dynamics','hybrid'},...
-    {'symbolic','casadi'});
+
+nlp = NLP(rbm,...
+    {'NFE', 25},...
+    {'CollocationScheme', 'HermiteSimpson'},... %HermiteSimpson,Trapezoidal
+    {'LinearSolver', 'mumps'},... %'ma57'},..
+    {'ConstraintTolerance', 1E-4});
+
+
+% Create functions for dynamics equations 
+nlp = ConfigFunctions(nlp, rbm);
+
+
+% Virtual constraint
+nlp = AddVirtualConstraints(nlp, rbm,...
+    {'PolyType', 'Bezier'},...
+    {'PolyOrder', 5},...
+    {'PolyPhase', 'time-based'});
+
+
+
+% load user-defined constraints
+[nlp, rbm] = LoadConstraints(nlp, rbm);
+
+
+
+% load seed
+%[nlp, rbm] = LoadSeed(nlp, rbm);
+[nlp, rbm] = LoadSeed(nlp, rbm, 'spatial-5link-seed.mat');
+ 
+
+
+nlp = ParseNLP(nlp, rbm);
+
+
+
+%% SOLVE
+
+nlp = SolveNLP(nlp);
+
+
+%% EXTRACT SOLUTION
+
+% structure containing the solution data
+data = ExtractData(nlp, rbm)
+
+
+%% ANIMATE SEED
+
+qAnim = rbm.States.q.Seed;
+tAnim = linspace(0, nlp.Problem.FinalTime.UpperBound-nlp.Problem.FinalTime.LowerBound, nlp.Settings.ncp);
+
+
+%% REFINE BOUNDS
+
+qAnim = [0.2, 0.2, 1.0, 0.0, 0.0, 0.0, -0.2, -0.8, 0.3, 0.2, -0.4, 0.3]';
+tAnim = 0;
+
+
+%% FLIP MAP
+
+R = Model.RelabelingMatrix();
+qAnim = R*qAnim;
+
+
+%% ANIMATE SOLUTION
+
+qAnim = data.pos;
+tAnim = data.t;
+
+
+%% ANIMATE
+
+% options: true or false 
+anim_options.bool = true;
+
+anim_options.axis.x = [-0.1 0.6];
+anim_options.axis.y = [-0.1 0.4];
+anim_options.axis.z = [0 1.3];
+
+% skips frame to animate faster 
+anim_options.skip_frame = 1;
+
+% views to show. Options: {'3D','frontal','sagittal','top'}
+anim_options.views = {'3D','frontal','sagittal','top'};
+%anim_options.views = {'3D'};
+
+% save options
+anim_options.save_movie    = false;
+anim_options.movie_name    = 'five_link.mp4';
+anim_options.movie_quality = 100; % scalar between [0 100], default 75
+anim_options.movie_fps     = 30;  % frame rate, default 30
+
+% create a light object or not
+anim_options.lights = true;
+
+% can pass figure as 5th argument
+Anim.animate(rbm, tAnim, qAnim, anim_options)
+%set(gcf,'menubar','figure')
+%set(gcf,'menubar','none')
+
+
+%% SAVE SEED
+
+seed.q = data.pos;
+seed.qd = data.vel;
+seed.qdd = data.acc;
+seed.t = data.t;
+seed.Fc_1 = data.Fc1;
+seed.a = data.a;
+seed.u = data.input;
+%seed.du = data.der_input;
+
+% can be used as seed
+str2save = 'spatial-5link-seed.mat';
+save(str2save, 'seed')
+
+
+
+
+
+
+%%
+
+q_test = [0.2, 0.2, 1.0, 0.05, 0.5, 0.2, -0.2, -0.8, 0.3, -0.2, -0.4, 0.3];
+
+
+% options: true or false 
+anim_options.bool = true;
+
+anim_options.axis.x = [-0.1 0.6];
+anim_options.axis.y = [-0.1 0.4];
+anim_options.axis.z = [0 1.3];
+
+% skips frame to animate faster 
+anim_options.skip_frame = 1;
+
+% views to show. Options: {'3D','frontal','sagittal','top'}
+anim_options.views = {'3D','frontal','sagittal','top'};
+%anim_options.views = {'3D'};
+
+% save options
+anim_options.save_movie    = false;
+anim_options.movie_name    = 'five_link.mp4';
+anim_options.movie_quality = 100; % scalar between [0 100], default 75
+anim_options.movie_fps     = 30;  % frame rate, default 30
+
+% create a light object or not
+anim_options.lights = true;
+
+% can pass figure as 5th argument
+Anim.animate( rbm , 0 , q_test , anim_options )
+%set(gcf,'menubar','figure')
+%set(gcf,'menubar','none')
+
+qn = q_test;
+
+
+%% get impact map
+
+q_guard = [qn(1:3),-qn(4),qn(5),-qn(6),-qn(10),qn(11:12),-qn(7),qn(8:9)];
+
+% can pass figure as 5th argument
+Anim.animate( rbm , 0 , q_guard , anim_options )
+
+
+
+
+%%
+
+
+R = [1,0,0,0,0,0,0,0,0,0,0,0;
+     0,1,0,0,0,0,0,0,0,0,0,0;
+     0,0,1,0,0,0,0,0,0,0,0,0;
+     0,0,0,-1,0,0,0,0,0,0,0,0;
+     0,0,0,0,1,0,0,0,0,0,0,0;
+     0,0,0,0,0,-1,0,0,0,0,0,0;
+     0,0,0,0,0,0,0,0,0,-1,0,0;
+     0,0,0,0,0,0,0,0,0,0,1,0;
+     0,0,0,0,0,0,0,0,0,0,0,1;
+     0,0,0,0,0,0,-1,0,0,0,0,0;
+     0,0,0,0,0,0,0,1,0,0,0,0;
+     0,0,0,0,0,0,0,0,1,0,0,0];
+     
+q_guard = R*qn'
+
+% can pass figure as 5th argument
+Anim.animate( rbm , 0 , q_guard , anim_options )
+
+
+%% Extract
+
+
+nlp
+size(nlp.VarsName,2)
+sol
+x_opt = full(sol.x);
+
+sol.pos = [];
+sol.vel = [];
+sol.acc = [];
+sol.input = [];
+sol.cForce = [];
+sol.tf = [];
+
+for i = 1:size(nlp.VarsName,2)
+    i
+    %nlp.VarsName{i}
+    %x_opt(nlp.VarsIdx{i})
+        
+    sol.(nlp.VarsName{i}) = [sol.(nlp.VarsName{i}), x_opt(nlp.VarsIdx{i})];
+
+
+end
+
+%%
+
+t = linspace(0, sol.tf, size(sol.pos,2))
+
+% options: true or false 
+anim_options.bool = true;
+
+anim_options.axis.x = [-0.1 0.6];
+anim_options.axis.y = [-0.1 0.4];
+anim_options.axis.z = [0 1.3];
+
+% skips frame to animate faster 
+anim_options.skip_frame = 1;
+
+% views to show. Options: {'3D','frontal','sagittal','top'}
+anim_options.views = {'3D','frontal','sagittal','top'};
+%anim_options.views = {'3D'};
+
+% save options
+anim_options.save_movie    = false;
+anim_options.movie_name    = 'five_link.mp4';
+anim_options.movie_quality = 100; % scalar between [0 100], default 75
+anim_options.movie_fps     = 30;  % frame rate, default 30
+
+% create a light object or not
+anim_options.lights = true;
+
+% can pass figure as 5th argument
+Anim.animate(rbm , t , sol.pos , anim_options )
+%set(gcf,'menubar','figure')
+%set(gcf,'menubar','none')
+
+
+
+
 
 
 %% minimal coordinates
@@ -99,10 +371,6 @@ write_file = true;
 init_report( problem , write_file )
 
 
-%% EXTRACT SOLUTION
-
-% struct containing the solution data
-data = Optim.extract_data( sol , problem , nlp , rbm , casadi , seed );
 
 
 %% PLOT SOLUTION  
