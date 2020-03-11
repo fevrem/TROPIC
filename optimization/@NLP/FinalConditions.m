8b0ed6cdcb6cf0896de78fb98c0885ac7aebe5a7
Martin Fevre <fevremartin2@gmail.com>
2020-03-11 12:35:08 -0400

    Create FinalConditions.m

---
+++ optimization/@NLP/FinalConditions.m
@@ -0,0 +1,27 @@
+function [nlp] = FinalConditions(nlp, rbm, grid_var)
+
+%% Argument Validation
+arguments
+    nlp (1,1) NLP
+    rbm (1,1) DynamicalSystem
+    grid_var (1,1) struct
+    %T (1,:) char {mustBeMember(T, {'t_minus','t_plus'})}
+end
+
+
+if nlp.Problem.FinalPositions.Bool
+    
+    [nlp] = add_constraint(nlp, grid_var.(['pos_', num2str(nlp.Settings.ncp)]), nlp.Problem.FinalPositions.LowerBound, nlp.Problem.FinalPositions.UpperBound, 'Final Position');      
+    
+end
+
+
+if nlp.Problem.FinalVelocities.Bool
+    
+    [nlp] = add_constraint(nlp, grid_var.(['vel_', num2str(nlp.Settings.ncp)]), nlp.Problem.FinalVelocities.LowerBound, nlp.Problem.FinalVelocities.UpperBound, 'Final Velocity');      
+    
+end
+
+
+    
+end
