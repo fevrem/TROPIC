8ba67c19a03a355801578a210d497528210bc597
Martin Fevre <fevremartin2@gmail.com>
2020-03-11 15:56:35 -0400

    Update spatial_12_dof_biped.m

--- examples/spatial-12-dof-biped/+Model/spatial_12_dof_biped.m
+++ examples/spatial-12-dof-biped/+Model/spatial_12_dof_biped.m
@@ -3,16 +3,12 @@ function  robot = spatial_12_dof_biped()
 %   to coordinate system of the predecessor frame of joint i
 
 
-
 import casadi.*
 
 robot.name = '12 DOF 5-Link Spatial Biped';
 
 % number of DOF
 robot.nd = 12;
-% robot.q   = SX.sym('',robot.nd);
-% robot.qd  = SX.sym('',robot.nd);
-% robot.qdd = SX.sym('',robot.nd);
 
 % geometric and inertial parameters
 model_prms = Model.model_params();
@@ -25,78 +21,38 @@ r_joint = 0.015;
     floating base
 %}
 robot.parent(1) = 0;  
-robot.body_axis{1} = [0 0 0];
-robot.body_length{1} = 0;
 robot.Xtree{1} = eye(6);
 robot.jtype{1}  = 'Px';
 robot.I{1} = zeros(6);
 robot.appearance.body{1} = Anim.add_appearance('base', [], [], false);
-%robot.q(1)   = SX.sym('x_h');
-%robot.qd(1)  = SX.sym('dx_h');
-%robot.qdd(1) = SX.sym('ddx_h');
-%robot.q(1) = Var('x_h', 1);
-%robot.qd(1) = Var('dx_h', 1);
-%robot.qdd(1) = Var('ddx_h', 1);
+
 
 robot.parent(2) = 1; 
-robot.body_axis{2} = [0 0 0];
-robot.body_length{2} = 0;
 robot.Xtree{2} = eye(6);
 robot.jtype{2}  = 'Py';
 robot.I{2} = zeros(6);
 robot.appearance.body{2} = Anim.add_appearance('base', [], [], false);
-%robot.q(2)   = SX.sym('y_h');
-%robot.qd(2)  = SX.sym('dy_h');
-%robot.qdd(2) = SX.sym('ddy_h');
-%robot.q(2) = Var('y_h', 1);
-%robot.qd(2) = Var('dy_h', 1);
-%robot.qdd(2) = Var('ddy_h', 1);
 
 
 robot.parent(3) = 2;    
-robot.body_axis{3} = [0 0 0];
-robot.body_length{3} = 0;
 robot.Xtree{3} = eye(6);
 robot.jtype{3}  = 'Pz';
 robot.I{3} = zeros(6);
 robot.appearance.body{3} = Anim.add_appearance('base', [], [], false);
-%robot.q(3)   = SX.sym('z_h');
-%robot.qd(3)  = SX.sym('dz_h');
-%robot.qdd(3) = SX.sym('ddz_h');
-%robot.q(12) = Var('q_k1', 1);
-%robot.qd(12) = Var('dq_k1', 1);
-%robot.qdd(12) = Var('ddq_k1', 1);
+
 
 robot.parent(4) = 3;   
-robot.body_axis{4} = [0 0 0];
-robot.body_length{4} = 0;
 robot.Xtree{4} = eye(6);
 robot.jtype{4}  = 'Rx';
 robot.I{4} = zeros(6);
 robot.appearance.body{4} = Anim.add_appearance('base', [], [], false);
-%robot.q(4)   = SX.sym('phi_x');
-%robot.qd(4)  = SX.sym('dphi_x');
-%robot.qdd(4) = SX.sym('ddphi_x');
-%robot.q(12) = Var('q_k1', 1);
-%robot.qd(12) = Var('dq_k1', 1);
-%robot.qdd(12) = Var('ddq_k1', 1);
-%robot.q(12) = Var('q_k1', 1);
-%robot.qd(12) = Var('dq_k1', 1);
-%robot.qdd(12) = Var('ddq_k1', 1);
+
 
 robot.parent(5) = 4;    
-robot.body_axis{5} = [0 0 0];
-robot.body_length{5} = 0;
 robot.Xtree{5} = eye(6);
 robot.jtype{5}  = 'Ry';
 robot.I{5} = zeros(6);
 robot.appearance.body{5} = Anim.add_appearance('base', [], [], false);
-% robot.q(5)   = SX.sym('phi_y');
-% robot.qd(5)  = SX.sym('dphi_y');
-% robot.qdd(5) = SX.sym('ddphi_y');
-% robot.q(12) = Var('q_k1', 1);
-% robot.qd(12) = Var('dq_k1', 1);
-% robot.qdd(12) = Var('ddq_k1', 1);
 
 % torso
 robot.parent(6) = 5;    
@@ -126,12 +82,7 @@ body_appearance{3}.axis = [0 -1 0];
 body_appearance{3}.color = model_prms.C_torso;
 body_appearance{3}.radius = r_link;
 robot.appearance.body{6} = Anim.add_appearance('slender-rod', body_appearance, joint_appearance, false);
-% robot.q(6)   = SX.sym('phi_z');
-% robot.qd(6)  = SX.sym('dphi_z');
-% robot.qdd(6) = SX.sym('ddphi_z');
-% robot.q(12) = Var('q_k1', 1);
-% robot.qd(12) = Var('dq_k1', 1);
-% robot.qdd(12) = Var('ddq_k1', 1);
+
 
 % qj1
 clear body_appearance joint_appearance
@@ -142,12 +93,7 @@ robot.Xtree{7} = plux(eye(3), model_prms.W_pelvis/2*[0 1 0]);
 robot.jtype{7}  = 'Rx';
 robot.I{7} = zeros(6);
 robot.appearance.body{7} = Anim.add_appearance('joint', [], [], false);
-% robot.q(7)   = SX.sym('q_abad1');
-% robot.qd(7)  = SX.sym('dq_abad1');
-% robot.qdd(7) = SX.sym('ddq_abad1');
-% robot.q(12) = Var('q_k1', 1);
-% robot.qd(12) = Var('dq_k1', 1);
-% robot.qdd(12) = Var('ddq_k1', 1);
+
 
 % qj2
 clear body_appearance joint_appearance
@@ -165,12 +111,7 @@ joint_appearance{1}.radius = r_joint;
 joint_appearance{1}.axis = robot.body_axis{8};
 joint_appearance{1}.distance = 0;
 robot.appearance.body{8} = Anim.add_appearance('slender-rod', body_appearance, joint_appearance, false);
-% robot.q(8)   = SX.sym('q_th1');
-% robot.qd(8)  = SX.sym('dq_th1');
-% robot.qdd(8) = SX.sym('ddq_th1');
-% robot.q(12) = Var('q_k1', 1);
-% robot.qd(12) = Var('dq_k1', 1);
-% robot.qdd(12) = Var('ddq_k1', 1);
+
 
 % qj3
 clear body_appearance joint_appearance
@@ -191,12 +132,7 @@ joint_appearance{2}.radius = r_joint;
 joint_appearance{2}.axis = robot.body_axis{9};
 joint_appearance{2}.distance = model_prms.L_lower_leg;
 robot.appearance.body{9} = Anim.add_appearance('slender-rod', body_appearance, joint_appearance, false);
-% robot.q(9)   = SX.sym('q_k1');
-% robot.qd(9)  = SX.sym('dq_k1');
-% robot.qdd(9) = SX.sym('ddq_k1');
-% robot.q(12) = Var('q_k1', 1);
-% robot.qd(12) = Var('dq_k1', 1);
-% robot.qdd(12) = Var('ddq_k1', 1);
+
 
 % qj4
 clear body_appearance joint_appearance
@@ -207,12 +143,6 @@ robot.Xtree{10} = plux(eye(3), model_prms.W_pelvis/2*[0 -1 0]);
 robot.jtype{10}  = 'Rx';
 robot.I{10} = zeros(6);
 robot.appearance.body{10} = Anim.add_appearance('joint', [], [], false);
-% robot.q(10)   = SX.sym('q_abad2');
-% robot.qd(10)  = SX.sym('dq_abad2');
-% robot.qdd(10) = SX.sym('ddq_abad2');
-% robot.q(12) = Var('q_k1', 1);
-% robot.qd(12) = Var('dq_k1', 1);
-% robot.qdd(12) = Var('ddq_k1', 1);
 
 % qj5
 clear body_appearance joint_appearance
@@ -230,12 +160,7 @@ joint_appearance{1}.radius = r_joint;
 joint_appearance{1}.axis = robot.body_axis{11};
 joint_appearance{1}.distance = 0;
 robot.appearance.body{11} = Anim.add_appearance('slender-rod', body_appearance, joint_appearance, false);
-% robot.q(11)   = SX.sym('q_th2');
-% robot.qd(11)  = SX.sym('dq_th2');
-% robot.qdd(11) = SX.sym('ddq_th2');
-% robot.q(12) = Var('q_k1', 1);
-% robot.qd(12) = Var('dq_k1', 1);
-% robot.qdd(12) = Var('ddq_k1', 1);
+
 
 % qj6
 clear body_appearance joint_appearance
@@ -256,9 +181,6 @@ joint_appearance{2}.radius = r_joint;
 joint_appearance{2}.axis = robot.body_axis{12};
 joint_appearance{2}.distance = model_prms.L_lower_leg;
 robot.appearance.body{12} = Anim.add_appearance('slender-rod', body_appearance, joint_appearance, false);
-% robot.q(12)   = SX.sym('q_k1');
-% robot.qd(12)  = SX.sym('dq_k1');
-% robot.qdd(12) = SX.sym('ddq_k1');
 
 
 robot.q = Var('q', 12);
@@ -269,16 +191,6 @@ robot.qdd = Var('ddq', 12);
 % floor tiles
 robot.appearance.base = {'tiles', [-10 10;-10 10; 0 0], .5};            
 
-% % contact definition: used to derive contact Jacobian(s)
-% robot.contact.type = 'point'; % only support point contact so far
-% robot.contact.points = [9,12]; % points = [i,...] means distal end of body(i)
-% 
-
-% contact definition: used to derive contact Jacobian(s)
-%robot.contact(1).type = 'point'; % only support point contact so far (can be point, line, plane)
-%robot.contact(1).points = 9; % points = [i,...] means distal end of body(i)
-%robot.contact(2).type = 'point';
-%robot.contact(2).points = 12; % points = [i,...] means distal end of body(i)
 
 
 % input torques
