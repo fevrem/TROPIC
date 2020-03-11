4a8dc1dad641517c26af16b26f36a3d712768a79
Martin Fevre <fevremartin2@gmail.com>
2020-03-11 12:35:06 -0400

    Create FootHeight.m

---
+++ optimization/@NLP/FootHeight.m
@@ -0,0 +1,58 @@
+function [nlp] = FootHeight(nlp, rbm, grid_var)
+
+
+%% Argument Validation
+arguments
+    nlp (1,1) NLP
+    rbm (1,1) DynamicalSystem
+    grid_var (1,1) struct
+    %T (1,:) char {mustBeMember(T, {'t_minus','t_plus'})}
+end
+
+
+
+% ground clearance 
+if nlp.Problem.GroundClearance.Bool
+
+    for i = 1:nlp.Settings.ncp
+
+        % swing foot height 
+        if nlp.Problem.SwingFootHeight.Bool
+            idx_cp = nlp.Problem.SwingFootHeight.Timing;
+            if idx_cp == i
+                mustBeInteger(idx_cp)
+                mustBeGreaterThan(idx_cp,1)
+                mustBeLessThan(idx_cp,nlp.Settings.ncp)
+
+                [nlp] = add_constraint(nlp, nlp.Problem.SwingFootHeight.Function(grid_var.(['pos_', num2str(idx_cp)])), nlp.Problem.SwingFootHeight.LowerBound, nlp.Problem.SwingFootHeight.UpperBound, [nlp.Problem.SwingFootHeight.Name, '@', num2str(idx_cp)]);
+                continue; % do not enforce redundant constraints
+            end
+
+        end
+
+        [nlp] = add_constraint(nlp, nlp.Problem.GroundClearance.Function(grid_var.(['pos_', num2str(i)])), nlp.Problem.GroundClearance.LowerBound, nlp.Problem.GroundClearance.UpperBound, [nlp.Problem.GroundClearance.Name, '@', num2str(i)]);
+
+    end
+
+% not sure why it would make sense to have swing foot height but not ground clearance    
+else
+
+    % swing foot height 
+    if nlp.Problem.SwingFootHeight.Bool
+        idx_cp = nlp.Problem.SwingFootHeight.Timing;
+        mustBeInteger(idx_cp)
+        mustBeGreaterThan(idx_cp,1)
+        mustBeLessThan(idx_cp,nlp.Settings.ncp)
+
+        [nlp] = add_constraint(nlp, nlp.Problem.SwingFootHeight.Function(grid_var.(['pos_', num2str(idx_cp)])), nlp.Problem.SwingFootHeight.LowerBound, nlp.Problem.SwingFootHeight.UpperBound, [nlp.Problem.SwingFootHeight.Name, '@', num2str(idx_cp)]);
+
+    end
+
+
+end
+
+
+
+
+end
+
