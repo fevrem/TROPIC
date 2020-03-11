bb223b38d96fe36a05a6b3bcd0d2fee8971fa879
Martin Fevre <fevremartin2@gmail.com>
2020-03-11 12:32:22 -0400

    Create add_coordinate_sys.m

---
+++ +Anim/add_coordinate_sys.m
@@ -0,0 +1,37 @@
+function add_coordinate_sys(rbm)
+
+
+        % calculate size of arrows for coordinate system
+        maxAx = max( [ range(xlim) , range(ylim) , range(zlim) ] );
+        
+%         switch rbm.dynamics
+%             case 'hybrid'
+                percent_of_fig = 0.10;
+                %percent_inside = 0.2;
+%         
+%             case 'continuous'
+%                 percent_of_fig = 0.07;
+%                 percent_inside = 0.1;
+%         end
+        
+        Larrow = percent_of_fig*maxAx;
+        
+        origin_arrow = [0,0,0];
+        
+        xLi = xlim;
+        yLi = ylim;
+        
+        %origin_arrow(1) = xLi(1) + percent_inside*range(xLi);
+        %origin_arrow(2) = yLi(1) + percent_inside*range(yLi);
+        
+        Anim.mArrow3(origin_arrow,origin_arrow+[Larrow,0,0],'color','b','stemWidth',Larrow/30,'tipWidth',Larrow/15);
+        Anim.mArrow3(origin_arrow,origin_arrow+[0,Larrow,0],'color','g','stemWidth',Larrow/30,'tipWidth',Larrow/15);
+        Anim.mArrow3(origin_arrow,origin_arrow+[0,0,Larrow],'color','r','stemWidth',Larrow/30,'tipWidth',Larrow/15);
+       
+        
+        
+        
+
+end
+
+
