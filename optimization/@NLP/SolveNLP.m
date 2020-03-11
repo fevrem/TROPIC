8fb517c7a2aef2562df41b20263a7c4297fd2a6d
Martin Fevre <fevremartin2@gmail.com>
2020-03-11 12:34:20 -0400

    Create SolveNLP.m

---
+++ optimization/@NLP/SolveNLP.m
@@ -0,0 +1,28 @@
+function [obj] = SolveNLP(obj)%, rbm)
+
+arguments
+    obj (1,1) NLP
+end
+
+
+import casadi.*
+
+% structure the nlp
+prob = struct('f', obj.Cost, 'x', vertcat(obj.Vars{:}), 'g', vertcat(obj.Constr{:}));
+
+% select the linear solver and sets options
+[options] = IPOPToptions(obj)%obj)%, rbm)
+
+
+% send the nlp to ipopt
+solver = nlpsol('solver', 'ipopt', prob, options);
+
+% solve the NLP
+tic
+sol = solver('x0', obj.VarsInit, 'lbx', obj.VarsLB, 'ubx', obj.VarsUB,...
+            'lbg', obj.ConstrLB, 'ubg', obj.ConstrUB);
+toc
+
+obj.Sol = sol;
+
+end
